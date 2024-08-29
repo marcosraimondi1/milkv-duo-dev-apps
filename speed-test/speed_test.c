d30becf2d89b30a897da77402bd64599ac024664
marcosraimondi1 <marcosraimondi1@hotmail.com>
2024-08-29 00:57:35 -0300

    simplified code

--- speed-test/speed_test.c
+++ speed-test/speed_test.c
@@ -7,7 +7,7 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
-#define MESSAGE_SIZE       4080 /* maximum payload size (rpmsg mtu)*/
+#define MESSAGE_SIZE       4080 /* maximum payload size (rpmsg mtu) */
 #define NETLINK_TEST       17
 #define NUM_MESSAGES       2300
 #define LOADING_BAR_LENGTH 50
@@ -16,118 +16,106 @@ void print_loading_bar(int progress);
 double elapsed_secs(struct timeval start, struct timeval end);
 void calculate_and_print_results(int msg_size, double time_taken, int num_messages);
 
-int main()
+int main(int argc, char *argv[])
 {
 	struct sockaddr_nl src_addr, dest_addr;
 	struct nlmsghdr *nlh;
-	struct msghdr msg;
-	struct iovec iov;
 	int sock_fd, rc;
 	struct timeval start, end;
 	char send_buff[MESSAGE_SIZE];
 
-	for (int i = 0; i < MESSAGE_SIZE; i++) {
-		send_buff[i] = 'a';
+	int num_msgs = NUM_MESSAGES;
+	if (argc > 1) {
+		num_msgs = atoi(argv[1]);
+		if (num_msgs <= 0) {
+			printf("Invalid number of messages\n");
+			return 1;
+		}
 	}
+
+	// Initialize send buffer with 'a's
+	memset(send_buff, 'a', MESSAGE_SIZE - 1);
 	send_buff[MESSAGE_SIZE - 1] = '\0';
 
+	// Create netlink socket
 	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
 	if (sock_fd < 0) {
 		printf("socket: %s\n", strerror(errno));
 		return 1;
 	}
 
+	// Bind socket
 	memset(&src_addr, 0, sizeof(src_addr));
 	src_addr.nl_family = AF_NETLINK;
 	src_addr.nl_pid = getpid();
-	src_addr.nl_groups = 0;
 	if (bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr)) != 0) {
 		printf("bind: %s\n", strerror(errno));
 		close(sock_fd);
 		return 1;
 	}
 
+	// Set destination address
 	memset(&dest_addr, 0, sizeof(dest_addr));
 	dest_addr.nl_family = AF_NETLINK;
 	dest_addr.nl_pid = 0; // For Linux Kernel
-	dest_addr.nl_groups = 0;
 
 	printf("\n------------------- TEST -------------------\n");
 	printf("Size: %d bytes\n", MESSAGE_SIZE);
-	printf("Number of messages: %d\n", NUM_MESSAGES);
+	printf("Number of messages: %d\n", num_msgs);
 
+	// Allocate and initialize netlink message header
 	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MESSAGE_SIZE));
 	nlh->nlmsg_len = NLMSG_SPACE(MESSAGE_SIZE);
 	nlh->nlmsg_pid = getpid();
-	nlh->nlmsg_flags = 0;
-
-	memset(send_buff, 'a', MESSAGE_SIZE - 1);
-	send_buff[MESSAGE_SIZE - 1] = '\0';
 	strcpy(NLMSG_DATA(nlh), send_buff);
 
-	memset(&iov, 0, sizeof(iov));
-	iov.iov_base = (void *)nlh;
-	iov.iov_len = nlh->nlmsg_len;
-
-	memset(&msg, 0, sizeof(msg));
-	msg.msg_name = (void *)&dest_addr;
-	msg.msg_namelen = sizeof(dest_addr);
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-
 	double total_time_secs = 0;
-	for (int j = 0; j < NUM_MESSAGES; j++) {
+	for (int j = 0; j < num_msgs; j++) {
 
 		gettimeofday(&start, NULL);
-		rc = sendmsg(sock_fd, &msg, 0);
+
+		// Send message
+		rc = sendto(sock_fd, nlh, nlh->nlmsg_len, 0, (struct sockaddr *)&dest_addr,
+			    sizeof(dest_addr));
 		if (rc < 0) {
-			printf("sendmsg(): %s\n", strerror(errno));
+			printf("sendto(): %s\n", strerror(errno));
 			close(sock_fd);
 			return 1;
 		}
 
-		rc = recvmsg(sock_fd, &msg, 0);
+		// Receive message
+		rc = recvfrom(sock_fd, nlh, nlh->nlmsg_len, 0, NULL, NULL);
 		if (rc < 0) {
-			printf("recvmsg(): %s\n", strerror(errno));
+			printf("recvfrom(): %s\n", strerror(errno));
 			close(sock_fd);
 			return 1;
 		}
+
 		gettimeofday(&end, NULL);
-
 		total_time_secs += elapsed_secs(start, end);
 
-		// print received message
+		// Check received message
 		char *recv_msg = (char *)NLMSG_DATA(nlh);
-
-		// received len
 		int recv_len = nlh->nlmsg_len - NLMSG_HDRLEN;
 
-		// compare received message with sent message
 		if (recv_len != MESSAGE_SIZE || memcmp(send_buff, recv_msg, MESSAGE_SIZE) != 0) {
 			printf("Error: received message is different from sent message\n");
-			printf("Received: %s of len %d\n", recv_msg, recv_len);
 			close(sock_fd);
 			return 1;
 		}
 
-		for (int i = 0; i < MESSAGE_SIZE; i++) {
-			send_buff[i] = 'a';
-		}
-		send_buff[MESSAGE_SIZE - 1] = '\0';
-		strcpy(NLMSG_DATA(nlh), send_buff);
-		nlh->nlmsg_pid = getpid();
-		nlh->nlmsg_len = NLMSG_SPACE(MESSAGE_SIZE);
-		nlh->nlmsg_flags = 0;
+		print_loading_bar((j + 1) * LOADING_BAR_LENGTH / num_msgs);
 
-		print_loading_bar((j + 1) * LOADING_BAR_LENGTH / NUM_MESSAGES);
+		nlh->nlmsg_len = NLMSG_SPACE(MESSAGE_SIZE);
+		nlh->nlmsg_pid = getpid();
+		strcpy(NLMSG_DATA(nlh), send_buff);
 	}
 
 	printf("\n");
 
-	calculate_and_print_results(MESSAGE_SIZE, total_time_secs, NUM_MESSAGES);
+	calculate_and_print_results(MESSAGE_SIZE, total_time_secs, num_msgs);
 
 	free(nlh);
-
 	close(sock_fd);
 	return 0;
 }
@@ -140,9 +128,7 @@ double elapsed_secs(struct timeval start, struct timeval end)
 void calculate_and_print_results(int msg_size, double time_taken_secs, int num_messages)
 {
 	double latency = time_taken_secs / num_messages;
-
-	// Throughput = number of messages * message size / time taken
-	double throughput = (msg_size * num_messages) / (time_taken_secs); // Bytes per second
+	double throughput = (msg_size * num_messages) / time_taken_secs;
 
 	printf("Latency: %.6f seconds per message\n", latency);
 	printf("Time taken: %.6f seconds\n", time_taken_secs);
@@ -151,20 +137,19 @@ void calculate_and_print_results(int msg_size, double time_taken_secs, int num_m
 
 void print_loading_bar(int progress)
 {
-	printf("\r["); // Return to the start of the line and print the opening bracket
-
-	printf("\033[42m"); // green
+	printf("\r[");
+	printf("\033[42m");
 
 	for (int i = 0; i < LOADING_BAR_LENGTH; i++) {
 		if (i < progress) {
-			printf(" "); // Print the | character to represent progress
+			printf(" ");
 		} else {
-			printf("\033[0m"); // normal
-			printf(" ");       // Print a space for the remaining part of the bar
+			printf("\033[0m");
+			printf(" ");
 		}
 	}
 
-	printf("\033[0m");              // normal
-	printf("] %d%%", progress * 2); // Print the percentage completed
-	fflush(stdout);                 // Ensure the output is immediately displayed
+	printf("\033[0m");
+	printf("] %d%%", progress * 2);
+	fflush(stdout);
 }
